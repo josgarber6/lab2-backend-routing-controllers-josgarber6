ad279129d88d3f9f5d7a4ca6c71d1161e8cec709
josgarber6 <josgarber6@alum.us.es>
2022-03-08 18:45:27 +0100

    Tirale

--- routes/RestaurantRoutes.js
+++ routes/RestaurantRoutes.js
@@ -1,9 +1,33 @@
 'use strict'
 const RestaurantController = require('../controllers/RestaurantController')
 const OrderController = require('../controllers/OrderController')
+const ProductController = require('../controllers/ProductController')
 
 module.exports = (options) => {
   const app = options.app
 
   // TODO: Include routes for restaurant described in the lab session wiki page.
+
+  app.route('/restaurants')
+    .get(RestaurantController.index)
+    // creación restaurante (CRUD FR1 owner)
+    .post(RestaurantController.create)
+
+  app.route('/restaurants/:restaurantId')
+    .get(RestaurantController.show)
+
+  app.route('/restaurants/:')
+  // actualización restaurante
+    .put(RestaurantController.update)
+
+    .delete(RestaurantController.destroy)
+
+  app.route('/restaurants/:restaurantId/orders')
+    .get(OrderController.indexRestaurant)
+
+  app.route('/restaurants/:restaurantId/products')
+    .get(ProductController.indexRestaurant)
+
+  app.route('restaurants/:restaurantId/analytics')
+    .get(OrderController.analytics)
 }
