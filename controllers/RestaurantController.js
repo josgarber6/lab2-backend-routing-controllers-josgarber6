daa6e5b04a8594a9404bd995d16d39abdf689fe3
josgarber6 <josgarber6@alum.us.es>
2022-03-08 19:21:55 +0100

    Falta código para acabar esto

--- controllers/RestaurantController.js
+++ controllers/RestaurantController.js
@@ -4,15 +4,37 @@ const Restaurant = models.Restaurant
 const Product = models.Product
 
 exports.index = async function (req, res) {
+  // Version corta
 
+  try {
+    const restaurants = await Restaurant.findAll()
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
-
 exports.indexOwner = function (req, res) {
   res.sendStatus(404)
 }
 
 exports.create = async function (req, res) {
-
+  const newRestaurant = Restaurant.build(req.body)
+  if (typeof req.files?.heroImage !== 'undefined') {
+    newRestaurant.heroImage = req.files.heroImage[0].path
+  }
+  if (typeof req.files?.logo !== 'undefined') {
+    newRestaurant.logo = req.files.logo[0].path
+  }
+  try {
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    if (err.name.includes('ValidationError')) { // The database may return some kind of error.
+      res.status(422).send(err)
+    } else {
+      res.status(500).send(err)
+    }
+  }
 }
 
 exports.show = async function (req, res) {
